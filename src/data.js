10405a3997a534f9df63f6f5bf76d6946f1d7c0d
ITVastian <sebiquesada11@gmail.com>
2024-11-14 17:27:26 -0300

    subiendo

---
+++ src/data.js
@@ -0,0 +1,51 @@
+export const imgs = [
+	{
+		id: 1,
+		img: '/img/facebook2.png',
+		alt: 'Facebook',
+	},
+	{
+		id: 2,
+		img: '/img/discord2.png',
+		alt: 'Discord',
+	},
+	{
+		id: 3,
+		img: '/img/instagram2.png',
+		alt: 'Instagram',
+	},
+	{
+		id: 4,
+		img: '/img/whatsapp2.png',
+		alt: 'WhatsApp',
+	},
+	{
+		id: 5,
+		img: '/img/google2.png',
+		alt: 'Google',
+	},
+	{
+		id: 6,
+		img: '/img/google_duo2.png',
+		alt: 'Google Duo',
+	},
+	{
+		id: 7,
+		img: '/img/linkedin2.png',
+		alt: 'Linkedin',
+	},
+	{
+		id: 8,
+		img: '/img/netflix2.png',
+		alt: 'Netflix',
+	},
+	{
+		id: 9,
+		img: '/img/messenger2.png',
+		alt: 'Messenger',
+	},
+	{   id: 10,
+        img: '/img/spotify2.png',
+        alt: 'Spotify'
+    },
+];
